e85f2af686c9e57e073146158f6988b96ed7f5ed
Lucas Rodiadis <lucas2@live.com.au>
2024-04-15 03:12:22 +1000

    Push react on regex

---
+++ commands/onmessage/reactOnRegex.ts
@@ -0,0 +1,19 @@
+import { Message } from "discord.js";
+import { MessageCommand } from "../../types";
+
+const REACTION_LIST: Array<{re: RegExp, reactions: Array<string>}> = [
+    {re: /a(ct|c?k?sh)ually/i, reactions: ["🤓"]},
+];
+
+const reactOnRegex: MessageCommand = (message: Message) => {
+    REACTION_LIST.forEach((reaction) => {
+        if (reaction.re.test(message.content)) {
+            reaction.reactions.forEach((emoji) => {
+                message.react(emoji);
+            });
+            
+        }
+    })
+}
+
+export default reactOnRegex;
