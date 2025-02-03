ba3e8bd7448dae9b8ea9d3589edad50d4da4f749
luboise <lucas2@live.com.au>
2025-02-03 23:17:09 +1100

    Added slight misclick mayhaps

--- commands/voice/onjoin/slightMisclick.ts
+++ commands/voice/onjoin/slightMisclick.ts
@@ -1,23 +1,50 @@
-import { ChannelType, VoiceChannel } from "discord.js";
+import { ChannelType, GuildMember, User, VoiceChannel } from "discord.js";
 import { VoiceCommand } from "../../../types";
 import randBool from "../../../utils/randBool";
 
-const MOVE_THRESHOLD = 0.5;
+const MOVE_THRESHOLD = 0.85;
+
+export const moveUserRecursive = async (
+	member: GuildMember,
+	index: number,
+	voiceChannels: VoiceChannel[],
+) => {
+	try {
+		const user = member.user;
+		if (!user) return;
+
+		//const voiceChannels = newState.channel.guild.channels.cache
+		//	.filter((c) => c.type === ChannelType.GuildVoice)
+		//	.sort((a, b) => a.position - b.position)
+		//	.toJSON();
+
+		if (!voiceChannels) return;
+
+		let newIndex = index - 1 + (randBool(0.5) ? 0 : 2);
+		if (newIndex < 0) newIndex = voiceChannels.length - 1;
+		newIndex %= voiceChannels.length;
+
+		const newVoiceChannel = voiceChannels[newIndex];
+
+		await member.voice.setChannel(newVoiceChannel);
+
+		if (randBool(MOVE_THRESHOLD)) {
+			setTimeout(() => {
+				moveUserRecursive(member, newIndex, voiceChannels);
+			}, 350);
+		}
+	} catch (error) {
+		console.error(error);
+		return;
+	}
+};
 
 const moveUser: VoiceCommand = (newState) => {
 	try {
-		const user = newState.member?.user;
-		if (!user) return;
-
 		const channelJoined = newState.channel;
-		if (!channelJoined) return;
+		if (!channelJoined || !newState.member) return;
 
 		if (randBool(MOVE_THRESHOLD)) {
-			//const voiceChannels = newState.channel.guild.channels.cache
-			//	.filter((c) => c.type === ChannelType.GuildVoice)
-			//	.sort((a, b) => a.position - b.position)
-			//	.toJSON();
-
 			const voiceChannels = (
 				Array.from(
 					newState.channel.guild.channels.cache
@@ -26,20 +53,12 @@ const moveUser: VoiceCommand = (newState) => {
 				) as VoiceChannel[]
 			).sort((a, b) => a.rawPosition - b.rawPosition);
 
-			if (!voiceChannels) return;
-
 			const index: number = voiceChannels.findIndex(
 				(val) => val.id === channelJoined.id,
 			);
 			if (index == -1) return;
 
-			let newIndex = index - 1 + (randBool(0.5) ? 0 : 2);
-			if (newIndex < 0) newIndex = voiceChannels.length - 1;
-			newIndex %= voiceChannels.length;
-
-			const newVoiceChannel = voiceChannels[newIndex];
-
-			newState.member.voice.setChannel(newVoiceChannel);
+			moveUserRecursive(newState.member, index, voiceChannels);
 		}
 	} catch (error) {
 		console.error(error);
--- utils/randBool.ts
+++ utils/randBool.ts
@@ -1,5 +1,5 @@
 function randBool(odds: number = 0.5): Boolean {
-	return Math.random() > odds;
+	return Math.random() < odds;
 }
 
 export default randBool;
